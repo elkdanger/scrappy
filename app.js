738e5d4a7c5cc70b559ce70fe9ec31d89b5dac22
Steve Hobbs <elkdanger@gmail.com>
2015-12-21 18:23:13 +0000

    Initial login form

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/login', require('./routes/login'))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
