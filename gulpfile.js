0393b95492f66f74dd1f122ccc1bee3eee652e95
Steve Hobbs <elkdanger@gmail.com>
2015-12-21 12:44:32 +0000

    Gulp + sass + sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,29 @@ let gulp = require('gulp')
 let plugins = require('gulp-load-plugins')()
 let browserSync = require('browser-sync').create()
 
+let sources = {
+    sass: 'public/**/*.scss'
+}
+
 /* Browser sync */
 gulp.task('browser-sync', () => {
 
   browserSync.init({
     proxy: 'localhost:3000',
-    port: 4202
+    port: 4202,
+    open: false
   })
 
 })
+
+gulp.task('sass', () =>    
+    gulp.src(sources.sass)
+        .pipe(plugins.sourcemaps.init())
+        .pipe(plugins.sass())
+        .pipe(plugins.sourcemaps.write())
+        .pipe(gulp.dest('public/'))
+        .pipe(browserSync.stream())
+)
 
 gulp.task('serve', ['browser-sync'], () => {
 
@@ -21,5 +35,10 @@ gulp.task('serve', ['browser-sync'], () => {
     ext: 'js hbs',
     env: { 'NODE_ENV': 'development' }
   })
+  
+  gulp.watch('views/**/*.hbs')
+    .on('change', browserSync.reload)
+    
+  gulp.watch(sources.sass, ['sass'])
 
 })
