2c24146aaa8cd66e7a5c27d981bb839a207ea6d5
Steve Hobbs <elkdanger@gmail.com>
2015-12-21 18:28:43 +0000

    Initial login form post

--- routes/login.js
+++ routes/login.js
@@ -4,9 +4,14 @@ let express = require('express')
 let router = express.Router();
 
 router.get('/', (req, res) => {
-    
     res.render('login')
+})
+
+router.post('/', (req, res) => {
     
+    // Check username and password
+    
+    res.render('login', req.body)
 })
 
 module.exports = router;
