e5da5263be747d51bca6eb8614de7909f9b2fa65
Steve Hobbs <elkdanger@gmail.com>
2015-12-21 12:28:24 +0000

    Added nodemon + browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -2,4 +2,24 @@
 
 let gulp = require('gulp')
 let plugins = require('gulp-load-plugins')()
+let browserSync = require('browser-sync').create()
 
+/* Browser sync */
+gulp.task('browser-sync', () => {
+
+  browserSync.init({
+    proxy: 'localhost:3000',
+    port: 4202
+  })
+
+})
+
+gulp.task('serve', ['browser-sync'], () => {
+
+  plugins.nodemon({
+    script: 'bin/www',
+    ext: 'js hbs',
+    env: { 'NODE_ENV': 'development' }
+  })
+
+})
